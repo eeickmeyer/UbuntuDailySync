473fbc678911810262c6748e05a1483ae921967f
Erich Eickmeyer <erich@ericheickmeyer.com>
2020-03-25 17:43:10 -0700

    Initial commit

---
+++ Ubuntu Budgie/sync-iso.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -f ../sync-dailies.config ]; then
+  . ../sync-dailies.config
+  OLDCODENAME=${CODENAME}
+else
+  CODENAME=$1
+  OLDCODENAME=${CODENAME}
+fi
+if [ "${CODENAME}" = "" ]; then
+  echo "No codename provided!"
+  exit 1
+fi
+if [ "${MIRROR}" = "" ]; then
+  MIRROR=http://cdimage.ubuntu.com
+fi
+zsync ${MIRROR}/ubuntu-budgie/daily-live/current/${CODENAME}-desktop-amd64.iso.zsync -i ./${OLDCODENAME}-desktop-amd64.iso
+rm *.zs-old
+
